02554b7ea76dca72638cbb0df52d5e9829371971
Sayan Sinha Roy <sayansinharoy25@gmail.com>
2021-09-22 18:29:56 +0530

    Commit

--- arr.java
+++ arr.java
@@ -2,6 +2,15 @@ public class arr {
     public static void main(String[] args) {
         int[] b = {2,5,9,0,3,12,7};
         int num = 7;
-        
+        for(int i = 0; i <= b.length; i++)
+        {
+            if(b[i] == num)
+            {
+                System.out.println(num);
+            }
+            else{
+                System.out.println("Not Found");
+            }
+        }
 }
 }
