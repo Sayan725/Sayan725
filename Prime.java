8ac88fbb831412f8a3cc6d0673d1518c160cc020
Sayan Sinha Roy <sayansinharoy25@gmail.com>
2021-09-22 18:12:22 +0530

    Commit

---
+++ Prime.java
@@ -0,0 +1,25 @@
+public class Prime {
+    public void prime()
+     {
+         
+         for(int i = 15; i < 80; i++)
+         {
+           int c = 0;
+           for(int j = 1; j <= i; j++)
+           {
+               if(i % j == 0)
+               c = c + 1;
+           }
+           if(c == 2)
+           {
+               System.out.println(i);
+           }
+         }
+     }
+    public static void main(String[] args) {
+
+        Prime p = new Prime();
+        p.prime();
+           
+    }
+}
