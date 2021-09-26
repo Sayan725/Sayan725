6e375b141e052bb5c8e1e62e05929544eca1d603
Sayan Sinha Roy <sayansinharoy25@gmail.com>
2021-09-26 15:22:51 +0530

    My Commit

---
+++ Sum.java
@@ -0,0 +1,18 @@
+public class Sum {
+    
+    public static int sum(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int sum(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+
+    public static double sum(double d, double e )
+    {
+        return d + e;
+    }
+
+}
